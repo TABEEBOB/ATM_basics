5c2dacbdf29942413a1fdca7811dd3fede106e84
Arrey Tabe Ebob <60329084+TABEEBOB@users.noreply.github.com>
2020-08-15 19:03:46 -0400

    Update InsufficientFundsException.java

--- InsufficientFundsException.java
+++ InsufficientFundsException.java
@@ -1,11 +1,7 @@
 
 package account;
 
-/**
- *  Michael Arrey
- * Lab #3
- * I Promise I wrote this code...
- */
+
 public class InsufficientFundsException extends Exception{
     private String Message = "Inssufficient Funds in Account...";
 
